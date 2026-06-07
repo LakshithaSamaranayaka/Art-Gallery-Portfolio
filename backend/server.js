401a3b83a63e1a307d598d969446fc7d56c2d67c
Lakshitha Samaranayaka <10952452@students.plymouth.ac.uk>
2026-06-07 22:24:01 +0530

    Fix backend deployement for vercel

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 app.use("/api/admin", adminRoutes);
 app.use("/api/artworks", artworkRoutes);
 
@@ -21,11 +22,9 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected successfully");
-
-    app.listen(process.env.PORT, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
-    });
   })
   .catch((error) => {
     console.log("MongoDB connection failed:", error.message);
   });
+
+module.exports = app;
